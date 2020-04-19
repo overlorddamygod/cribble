fff8c4b1c14d58dbd2306feb11bdac6b709c2978
overlorddamygod <overlord.damygod@gmail.com>
2020-04-20 00:55:39 +0545

    Added players

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-100 d-flex flex-column">
     <b-row class="w-100 top d-flex align-items-center">
-      <b-col cols="2">Time</b-col>
+      <b-col cols="2">Time {{`${turn.username}'s turn`}} Round: {{round}}</b-col>
       <b-col cols="9" class="text-center"><span class="options">{{optionWord}}</span></b-col>
       <b-col cols="1">
         <button 
@@ -14,17 +14,32 @@
     </b-row>
     <b-row class="box h-100">
       <b-col cols="1" class="d-flex flex-column">
-        <b-avatar
+        <div 
           v-for="player in players"
           :key="player.playerId"
-        />
+          class="d-flex"
+        >
+          <div>
+            <b-avatar :title="a"/>
+            <p>
+              {{player.username}}
+            </p>
+          </div>
+          <div>
+            <span v-if="player.playerId==turn.id"><img src="https://img.icons8.com/cute-clipart/64/000000/pencil.png"/></span>
+          </div>
+          
+        </div>
+
       </b-col>
       <b-col cols="7" class="rel">
-        <!-- <b-form-input 
-        placeholder="Enter the word"
-        v-model="word" 
-        @keyup.enter.prevent="sendWord"
-        /> -->
+        <button @click="startGame" v-if="!started">Start</button>
+        <b-form-input 
+          placeholder="Enter the word"
+          v-model="word" 
+          @keyup.enter.prevent="sendWord"
+          v-if="turn.id==id"
+        />
         <Whiteboard 
           :roomId="roomId"
           :image64="image64"
@@ -48,11 +63,15 @@ export default {
     Whiteboard
   },
   data:()=>({
+    id:'',
     word:'',
     optionWord:'',
     players:[],
     roomId:'',
-    image64:''
+    image64:'',
+    turn:'',
+    round:1,
+    started:false
   }),
   created() {
     this.roomId = this.$route.params.id
@@ -63,6 +82,9 @@ export default {
     this.$socket.client.emit('getPlayers',this.roomId)
   },
   methods: {
+    startGame() {
+      this.$socket.client.emit('start',this.roomId);
+    },
     sendWord() {
       if (!this.word) return 
       this.$socket.client.emit('sendWord',{
@@ -82,9 +104,30 @@ export default {
     playerDisconnected(player) {
       this.players = this.players.filter(({playerId})=> playerId != player.playerId)
     },
-    optionWord(optionWord) {
-      this.optionWord = optionWord
+    optionWord(word) {
+      if(this.turn.id ==this.id) {
+        this.optionWord = word.word
+      } else {
+        this.optionWord = word.optionWord
+      }
     },
+    timeUp(players) {
+      this.optionWord=''
+      console.log(players)
+    },
+    start(player) {
+      this.turn = player
+      // console.log(player,'turn')
+    },
+    rounds(round) {
+      this.round = round
+    },
+    started() {
+      this.started=true
+    },
+    myId(id) {
+      this.id = id
+    }
     // image(image64) {
     //   this.image64=image64
     // }
